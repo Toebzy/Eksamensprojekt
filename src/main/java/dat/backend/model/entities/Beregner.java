86f7ef4a35cc37579543d1007266a5d1744a2171
panksxd <panksxd@gmail.com>
2023-05-09 13:27:39 +0200

    beregner for brædder 100mm + test

--- src/main/java/dat/backend/model/entities/Beregner.java
+++ src/main/java/dat/backend/model/entities/Beregner.java
@@ -48,4 +48,83 @@ public class Beregner
         }
         return materialeList;
     }
+    public List<Materiale> trykImpBrædt100Beregner(double bredde, double længde)  //sætter først brædder ind til bredde derefter længde
+    {
+        List<Materiale> materialeList = new ArrayList<>();
+        int brædder540 = 0;
+        double overskydende = 0;
+        if(bredde >= 720)
+        {
+            brædder540 = (int) bredde/540;
+            for(int i = 0; i < brædder540; i++)
+            {
+                Materiale materiale = new Materiale(27, 540);
+                materialeList.add(materiale);
+            }
+            overskydende = bredde/540 - brædder540;
+            if(overskydende > 360)
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    Materiale materiale = new Materiale(26, 360);
+                    materialeList.add(materiale);
+                }
+            }
+            if(overskydende < 360)
+            {
+                materialeList.add(new Materiale(26, 360));
+            }
+        }
+        else if(bredde < 720 && bredde > 360)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                Materiale materiale = new Materiale(26, 360);
+                materialeList.add(materiale);
+            }
+        }
+        else if(bredde <= 360)
+        {
+            materialeList.add(new Materiale(26, 360));
+        }
+
+        if(længde >= 720)
+        {
+            brædder540 = (int) længde/540;
+            for(int i = 0; i < brædder540*2; i++)
+            {
+                Materiale materiale = new Materiale(27, 540);
+                materialeList.add(materiale);
+            }
+            overskydende = længde/540 - brædder540;
+            if(overskydende > 360)
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                    Materiale materiale = new Materiale(26, 360);
+                    materialeList.add(materiale);
+                }
+            }
+            if(overskydende < 360)
+            {
+                materialeList.add(new Materiale(26, 360));
+                materialeList.add(new Materiale(26, 360));
+            }
+        }
+        else if(længde < 720 && bredde > 360)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                Materiale materiale = new Materiale(26, 360);
+                materialeList.add(materiale);
+            }
+        }
+        else if(længde <= 360)
+        {
+            materialeList.add(new Materiale(26, 360));
+            materialeList.add(new Materiale(26, 360));
+        }
+        return materialeList;
+    }
+
 }
--- src/test/java/dat/backend/model/entities/BeregnerTest.java
+++ src/test/java/dat/backend/model/entities/BeregnerTest.java
@@ -17,18 +17,38 @@ class BeregnerTest
     void setUp()
     {
         beregner = new Beregner();
-        list.add(new Materiale(2, 540));
-        list.add(new Materiale(2, 540));
-        list.add(new Materiale(1, 360));
-        list.add(new Materiale(1, 360));
-
+        for(Materiale m: list)
+        {
+            list.remove(m);
+        }
     }
     @Test
     void testBrædder200()
     {
+        list.add(new Materiale(2, 540));
+        list.add(new Materiale(2, 540));
+        list.add(new Materiale(1, 360));
+        list.add(new Materiale(1, 360));
         assertEquals(list.get(0).getLength(), beregner.trykImpBrædt200Beregner(800).get(0).getLength());
         assertEquals(list.get(1).getLength(), beregner.trykImpBrædt200Beregner(800).get(1).getLength());
         assertEquals(list.get(2).getLength(), beregner.trykImpBrædt200Beregner(800).get(2).getLength());
         assertEquals(list.get(3).getLength(), beregner.trykImpBrædt200Beregner(800).get(3).getLength());
+    }
+
+    @Test
+    void testBrædder100()
+    {
+        list.add(new Materiale(27, 540));
+        list.add(new Materiale(26, 360));
+        list.add(new Materiale(27, 540));
+        list.add(new Materiale(27, 540));
+        list.add(new Materiale(26, 360));
+        list.add(new Materiale(26, 360));
+        assertEquals(list.get(0).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(0).getMvariant());
+        assertEquals(list.get(1).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(1).getMvariant());
+        assertEquals(list.get(2).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(2).getMvariant());
+        assertEquals(list.get(3).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(3).getMvariant());
+        assertEquals(list.get(4).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(4).getMvariant());
+        assertEquals(list.get(5).getMvariant(), beregner.trykImpBrædt100Beregner(800, 800).get(5).getMvariant());
     }
 }
