34b57fcc6be32e45b0d2b2217949ad365b5125aa
SimoneToft <kagefighter123@gmail.com>
2023-05-18 04:15:33 +0200

    Fixed duplicated code

--- src/main/java/dat/backend/model/entities/Calculator.java
+++ src/main/java/dat/backend/model/entities/Calculator.java
@@ -2,6 +2,8 @@ package dat.backend.model.entities;
 
 import dat.backend.model.persistence.ConnectionPool;
 import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.MaterialFacade;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.*;
@@ -35,16 +37,14 @@ public class Calculator {
         this.amountOfPoles = calculatePoleAmount(length, width);
         this.amountOfRafters = calculateRafterAmount(length,width);
 
-        this.fasciaAmount=calculateFasciaAmount(length, width);
+        this.fasciaAmount=calculateFasciaAmount(length, width, connectionPool);
         this.fascia=fasciaMaterial(fasciaAmount, connectionPool);
 
 
-        this.roofAmount = calculateRoofAmount(length, width);
+        this.roofAmount = calculateRoofAmount(length, width, connectionPool);
         this.roof = roofMaterial(roofAmount, connectionPool);
 
         this.totalPrice = getTotalPrice();
-
-
     }
 
     public static int calculateRafterAmount(int length, int width) {
@@ -52,52 +52,46 @@ public class Calculator {
     }
 
     public static int calculatePoleAmount(int length, int width) {
-        return (int) ((Math.ceil(length / 310)) * 2 + (Math.ceil(width / 310) * 2));
+        int maxDistance = 310;
+        return (int) ((Math.ceil(length / maxDistance)) * 2 + (Math.ceil(width / maxDistance) * 2));
     }
 
+    public static int[] calculateAmount(int id, int width, ConnectionPool connectionPool) throws DatabaseException {
+        List<Material> material= MaterialFacade.getMaterialById(id, connectionPool);
+        int length1 = (int) material.get(0).getLength();
+        int length2 = (int) material.get(1).getLength();
 
-    public static int[] calculateFasciaAmount(int length, int width) {
-        int fascia360=360;
-        int fascia540=540;
-        int[] optimalFasciaCounts = new int[2];
-        int[] optimalFasciaLengthCounts = new int[2];
-        int[] optimalFasciaWidthCounts = new int[2];
+        int[] optimalCounts = new int[2];
         int minWastage = Integer.MAX_VALUE;
 
-        for (int count360 = 0; count360 <= length / fascia360; count360++) {
-            for (int count540 = 0; count540 <= length / fascia540; count540++) {
-                int lengthCovered = count360 * fascia360 + count540 * fascia540;
-                int wastage = length - lengthCovered;
-
-                if (wastage >= 0 && wastage < minWastage) {
-                    minWastage = wastage;
-                    optimalFasciaLengthCounts[0] = count360;
-                    optimalFasciaLengthCounts[1] = count540;
-
-                    if (wastage > 0) {
-                        optimalFasciaLengthCounts[0]++;
-                    }
-                }
-            }
-        }
-        for (int count360 = 0; count360 <= width / fascia360; count360++) {
-            for (int count540 = 0; count540 <= width / fascia540; count540++) {
-                int widthCovered = count360 * fascia360 + count540 * fascia540;
+        for (int count1 = 0; count1 <= width / length1; count1++) {
+            for (int count2 = 0; count2 <= width / length2; count2++) {
+                int widthCovered = count1 * length1 + count2 * length2;
                 int wastage = width - widthCovered;
 
                 if (wastage >= 0 && wastage < minWastage) {
                     minWastage = wastage;
-                    optimalFasciaWidthCounts[0] = count360;
-                    optimalFasciaWidthCounts[1] = count540;
+                    optimalCounts[0] = count1;
+                    optimalCounts[1] = count2;
 
                     if (wastage > 0) {
-                        optimalFasciaWidthCounts[0]++;
+                        optimalCounts[0]++;
                     }
                 }
             }
         }
-        optimalFasciaCounts[0]=optimalFasciaWidthCounts[0]+optimalFasciaLengthCounts[0];
-        optimalFasciaCounts[1]=optimalFasciaWidthCounts[1]+optimalFasciaLengthCounts[1];
+        return optimalCounts;
+    }
+
+
+    public static int[] calculateFasciaAmount(int length, int width, ConnectionPool connectionPool) throws DatabaseException{
+
+        int[] optimalFasciaLengthCounts = calculateAmount(1 ,length,  connectionPool);
+        int[] optimalFasciaWidthCounts = calculateAmount(1 ,width,  connectionPool);
+        int[] optimalFasciaCounts = new int[2];
+
+        optimalFasciaCounts[0]=(optimalFasciaWidthCounts[0]+optimalFasciaLengthCounts[0])*2;
+        optimalFasciaCounts[1]=(optimalFasciaWidthCounts[1]+optimalFasciaLengthCounts[1])*2;
         return optimalFasciaCounts;
     }
 
@@ -107,10 +101,14 @@ public class Calculator {
         if (fasciaAmount[0] > 0 && fasciaAmount[1] > 0) {
             fasciaMaterials.add(Material.newMaterial(1, connectionPool));
             fasciaMaterials.add(Material.newMaterial(2, connectionPool));
+            fasciaMaterials.add(Material.newMaterial(4, connectionPool));
+            fasciaMaterials.add(Material.newMaterial(7, connectionPool));
         } else if (fasciaAmount[0] > 0) {
             fasciaMaterials.add(Material.newMaterial(1, connectionPool));
+            fasciaMaterials.add(Material.newMaterial(4, connectionPool));
         } else if (fasciaAmount[1] > 0) {
             fasciaMaterials.add(Material.newMaterial(2, connectionPool));
+            fasciaMaterials.add(Material.newMaterial(7, connectionPool));
         } else {
             throw new IllegalArgumentException("Invalid fascia combination.");
         }
@@ -118,7 +116,6 @@ public class Calculator {
     }
 
     public static List<Material> roofMaterial(int[] roofAmount, ConnectionPool connectionPool) {
-
         List<Material> RoofMaterials = new ArrayList<Material>();
         if (roofAmount[0] > 0 && roofAmount[1] > 0) {
             RoofMaterials.add(Material.newMaterial(29, connectionPool));
@@ -133,28 +130,9 @@ public class Calculator {
         return RoofMaterials;
     }
 
-    public static int[] calculateRoofAmount(int length, int width) {
-        int tile360=360;
-        int tile600=600;
-        int[] optimalTileCounts = new int[2];
-        int minWastage = Integer.MAX_VALUE;
+    public static int[] calculateRoofAmount(int length, int width, ConnectionPool connectionPool)throws DatabaseException {
+        int[] optimalTileCounts = calculateAmount(8 ,length,  connectionPool);
 
-        for (int count360 = 0; count360 <= length / tile360; count360++) {
-            for (int count600 = 0; count600 <= length / tile600; count600++) {
-                int lengthCovered = count360 * tile360 + count600 * tile600;
-                int wastage = length - lengthCovered;
-
-                if (wastage >= 0 && wastage < minWastage) {
-                    minWastage = wastage;
-                    optimalTileCounts[0] = count360;
-                    optimalTileCounts[1] = count600;
-
-                    if (wastage > 0) {
-                        optimalTileCounts[0]++;
-                    }
-                }
-            }
-        }
         optimalTileCounts[0]= (int) (optimalTileCounts[0]*Math.ceil(width/100));
         optimalTileCounts[1]= (int) (optimalTileCounts[1]*Math.ceil(width/100));
         return optimalTileCounts;
@@ -169,14 +147,17 @@ public class Calculator {
         map.put(pole, amountOfPoles);
         map.put(rafter, amountOfRafters);
 
-
         if (fasciaAmount[0] > 0 && fasciaAmount[1] > 0) {
             map.put(fascia.get(0),fasciaAmount[0]);
             map.put(fascia.get(1),fasciaAmount[1]);
+            map.put(fascia.get(2),fasciaAmount[0]);
+            map.put(fascia.get(3),fasciaAmount[1]);
         } else if (fasciaAmount[0] > 0) {
             map.put(fascia.get(0),fasciaAmount[0]);
+            map.put(fascia.get(1),fasciaAmount[0]);
         } else if (fasciaAmount[1] > 0) {
             map.put(fascia.get(0), fasciaAmount[1]);
+            map.put(fascia.get(1), fasciaAmount[1]);
         }
 
         if (roofAmount[0] > 0 && roofAmount[1] > 0) {
@@ -193,8 +174,9 @@ public class Calculator {
     public String toString() {
         Map<Material, Integer> partsList = getPartsList();
         StringBuilder sb = new StringBuilder();
+        sb.append("Stykliste for carport med længden: "+length+" og bredden: "+width+"\n");
         for (Map.Entry<Material, Integer> entry : partsList.entrySet()) {
-            sb.append(entry.getKey().getDescription()).append(": ").append(entry.getValue()).append("\n");
+            sb.append(entry.getKey().getDescription()).append("| Længde: ").append(entry.getKey().getLength()).append("| Mængde: ").append(entry.getValue()).append("\n");
         }
         return sb.toString();
     }
--- src/main/java/dat/backend/model/persistence/MaterialFacade.java
+++ src/main/java/dat/backend/model/persistence/MaterialFacade.java
@@ -4,9 +4,13 @@ import dat.backend.model.entities.Material;
 import dat.backend.model.exceptions.DatabaseException;
 
 import java.sql.Connection;
+import java.util.List;
 
-public class MaterialFacade
-{
+public class MaterialFacade{
+
+    public static List<Material> getMaterialById(int id, ConnectionPool connectionPool)throws DatabaseException {
+        return MaterialMapper.getMaterialById(id, connectionPool);
+    }
     public static Double getPrice(int mvariant, Connection connection) throws DatabaseException
     {
        return MaterialMapper.getPrice(mvariant, connection);
